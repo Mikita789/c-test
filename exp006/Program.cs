2054be0fa218cf6f3f880f72770106f9df4078d0
Mikita <nikita_popow_1993@mail.ru>
2022-12-14 13:33:18 +0300

    add exp006 if/else

---
+++ exp006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите свое имя");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "никита")
+{
+   Console.Write("Ну наконец ты вернулся!) Привет, ");
+   Console.WriteLine(userName);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
