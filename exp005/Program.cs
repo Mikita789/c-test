1f5562bbb88cd50fed2b02d30ff48866b966c281
Mikita <nikita_popow_1993@mail.ru>
2022-12-14 13:17:58 +0300

    add random number1 * random number2

---
+++ exp005/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = new Random().Next(1, 11);
+int number2 = new Random().Next(100,155);
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.Write("число 1 * число 2 = ");
+Console.WriteLine(number1 * number2);
