c28c0f928c91eb6d8b880054878f4715cd133242
Mikita <nikita_popow_1993@mail.ru>
2022-12-14 14:47:14 +0300

    add 2 fr and dog

---
+++ exp009/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int firstFrSp = 1;
+int secFrSp = 2;
+int dogSp = 5;
+int dist = 10000;
+int fr = 2;
+int time = 0;
+
+while(dist>10)
+{
+    if (fr == 1)
+    {
+        time = dist/(firstFrSp + dogSp);
+        fr = 2;
+    }
+    else
+    {
+        time = dist/(secFrSp + dogSp);
+        fr = 1;
+    }
+    dist = dist - (firstFrSp + secFrSp) * time;
+    count+=1;
+
+}
+
+Console.WriteLine(count);
