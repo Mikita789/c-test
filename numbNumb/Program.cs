555cb594a18596e39bb8fb0a09a62c4b6d69e8eb
Mikita <nikita_popow_1993@mail.ru>
2022-12-15 12:39:10 +0300

    number * number 2.0

--- numbNumb/Program.cs
+++ numbNumb/Program.cs
@@ -1,5 +1,18 @@
 ﻿// Квадрат числа 
-Console.WriteLine("Введите число для возведения в квадрат  ");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("Квадрат введенного числа равен   ");
-Console.WriteLine("Квадрат числа " + number + " будет равен " + number * number);
+//Console.WriteLine("Введите число для возведения в квадрат  ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Квадрат введенного числа равен   ");
+//Console.WriteLine("Квадрат числа " + number + " будет равен " + number * number);
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        Console.Write("Введенное число в квадрате = ");
+        Console.WriteLine(number*number);
+        break;
+    }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
