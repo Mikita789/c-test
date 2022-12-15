e6ffdd7115e6b2a907782c1418da2674766f98f5
Mikita <nikita_popow_1993@mail.ru>
2022-12-15 11:31:19 +0300

    add number * number

---
+++ numbNumb/Program.cs
@@ -0,0 +1,5 @@
+﻿// Квадрат числа 
+Console.WriteLine("Введите число для возведения в квадрат  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат введенного числа равен   ");
+Console.WriteLine("Квадрат числа " + number + " будет равен " + number * number);
