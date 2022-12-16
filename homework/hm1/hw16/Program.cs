3a2dff3117789b872b2d3b86e9be34456e0ab298
Mikita <nikita_popow_1993@mail.ru>
2022-12-16 11:05:24 +0300

    add HW1.6

--- homework/hm1/hw16/Program.cs
+++ homework/hm1/hw16/Program.cs
@@ -1,17 +1,35 @@
 ﻿// вводим номер дня недели - выводим строку 
-int dayNumber;
+//int dayNumber;
+string [] arrDays = new string[] {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
 
 while(true)
 {
-    Console.Write("Введите цифру дня недели");
+    Console.Write("Введите цифру дня недели от 1 до 7: ");
     string text = Console.ReadLine();
-    if (int.TryParse(text, out int number))
+    if ((int.TryParse(text, out int number)) && (Convert.ToInt32(text)>0 && Convert.ToInt32(text)<8))
     {
-        switch number (number)
+        switch(number)
         {
-            case 1:
-            
+            case 1: Console.WriteLine(arrDays[0]);
+            break;
+            case 2: Console.WriteLine(arrDays[1]);
+            break;
+            case 3: Console.WriteLine(arrDays[2]);
+            break;
+            case 4: Console.WriteLine(arrDays[3]);
+            break;
+            case 5: Console.WriteLine(arrDays[4]);
+            break;
+            case 6: Console.WriteLine(arrDays[5]);
+            break;
+            case 7: Console.WriteLine(arrDays[6]);
+            break;
         }
+        break;
 
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не удалось распознать");
     }
 }
