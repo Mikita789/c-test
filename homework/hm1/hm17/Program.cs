c376589a48ac779bdd40802adead2d33b7e845a1
Mikita <nikita_popow_1993@mail.ru>
2022-12-16 11:40:17 +0300

    add HW1.7

---
+++ homework/hm1/hm17/Program.cs
@@ -0,0 +1,18 @@
+﻿//на вход подается число N. на выход целые числа от -N до N
+
+while(true)
+{
+    Console.Write("Введите целое число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        for(int i = -number; i<=number; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Не удалось распознать число");
+    }
+}
