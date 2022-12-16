04cbe51cf732c2ad2f05d8307c1414d93c0f8ecd
Mikita <nikita_popow_1993@mail.ru>
2022-12-16 12:24:57 +0300

    add HW1.8

---
+++ homework/hm1/hm18/Program.cs
@@ -0,0 +1,18 @@
+﻿// на вход трехзначное число. вывести последнюю цифру числа
+
+while(true)
+{
+    Console.Write("Введите 3х значное число: ");
+    string text = Console.ReadLine();
+    if(int.TryParse(text, out int number))
+    {
+        Console.Write("Последняя цифра введенного числа = ");
+        Console.WriteLine(number.ToString()[^1]);
+        break;
+
+    }
+    else
+    {
+        Console.WriteLine("Правильно введите 3х значное число");
+    }
+}
