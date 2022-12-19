c430df279f8b30338c613a99769b2c7ec15d4173
Mikita <nikita_popow_1993@mail.ru>
2022-12-19 11:56:28 +0300

    add HW 010

---
+++ homework/hm2/seminar2/hw010/Program.cs
@@ -0,0 +1,21 @@
+﻿// задача 10
+
+int numb = 0;
+bool flag = true;
+
+while(flag)
+{
+    Console.Write("Введите 3х значное число: ");
+    string text = Console.ReadLine();
+    if(int.TryParse(text, out int number))
+    {
+        numb = number;
+        flag = false;
+        Console.WriteLine($"Вторая цифра числа : {numb.ToString()[1]}");
+    }
+    else
+    {
+        Console.WriteLine("Введите 3х значное число!!!");
+    }
+    
+}
