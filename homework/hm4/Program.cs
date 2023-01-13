a258b23e2d95d27bc5efe20c6f1da90dad1e5490
Mikita <nikita_popow_1993@mail.ru>
2023-01-13 11:16:02 +0300

    add HW4.27

--- homework/hm4/Program.cs
+++ homework/hm4/Program.cs
@@ -2,6 +2,8 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+
+//функция ввода цифры и ее проверки
 int CheckNumberInt (string message)
 {
     bool flag = true;
@@ -22,17 +24,46 @@ int CheckNumberInt (string message)
     return result;
 }
 
-int a = CheckNumberInt("Введите число 1 ");
-int b = CheckNumberInt("Введите число 2 ");
+// int a = CheckNumberInt("Введите число 1 ");
+// int b = CheckNumberInt("Введите число 2 ");
 
-int Exp (int x1, int x2)
-{
-    int result = x1;
-    for (int i=1; i < x2; i++)
-    {
-        result *= x1;
-    }
-    return result;
-}
-Console.Write($"Число А({a}) в степени числа B({b}): ");
-Console.WriteLine(Exp(a,b));
+// int Exp (int x1, int x2)
+// {
+//     int result = x1;
+//     for (int i=1; i < x2; i++)
+//     {
+//         result *= x1;
+//     }
+//     return result;
+// }
+// Console.Write($"Число А({a}) в степени числа B({b}): ");
+// Console.WriteLine(Exp(a,b));
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int a = CheckNumberInt("Введите число ");
+// //способ 1
+// int SumOfDigits(int x)
+// {
+//     int result = 0;
+//     string xs = x.ToString();
+//     for (int i = 0; i < xs.Length; i++)
+//     {
+//         result += int.Parse(xs[i].ToString());
+//     }
+//     return result;
+// }
+
+// //Console.WriteLine($"Cумма цифр числа({a}) равна {SumOfDigits(a)}");
+// // способ 2 OneLine
+// int SumOfDigits2(int x)
+// {
+//     return Array.ConvertAll(x.ToString().ToCharArray(),s => int.Parse(s.ToString())).Sum();
+// }
+// Console.WriteLine(SumOfDigits2(a));
+
+
