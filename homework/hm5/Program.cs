ff1516f137890221e95f9cb947bb8467e567db70
Mikita <nikita_popow_1993@mail.ru>
2023-01-16 16:07:14 +0300

    add all HW5

--- homework/hm5/Program.cs
+++ homework/hm5/Program.cs
@@ -61,27 +61,18 @@ void PrintArr<T>(ref T[] arr)
 
 //Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
-double[] RandomDoubleArr(int count, double startDouble, double finishDouble)
-{
-    double[] result = new double[count];
-    for ( int i = 0; i < count; i++)
-    {
-        result[i] = Math.Round(new Random().NextDouble() * (finishDouble - startDouble) + startDouble, 2);
-    }
-    return result;
-}
-double[] arr = RandomDoubleArr(10, 10.0, 100.0);
+int[] arr = RandomIntArr(10, 10, 100);
 PrintArr(ref arr);
 //способ 1
 //Console.WriteLine($"максимальный элемент = {arr.Max()}, минимальный элемент = {arr.Min()}, макс - мин = {arr.Max() - arr.Min()}");
 
 //способ 2
-// Array.Sort(arr);
-// Console.WriteLine($"максимальный элемент = {arr[^1]}, минимальный элемент = {arr[0]}, макс - мин = {arr[^1] - arr[0]}");
+//Array.Sort(arr);
+//Console.WriteLine($"максимальный элемент = {arr[^1]}, минимальный элемент = {arr[0]}, макс - мин = {arr[^1] - arr[0]}");
 
 //способ 3
-// double max = arr[0];
-// double min = arr[0];
+// int max = arr[0];
+// int min = arr[0];
 // for (int i = 0; i < arr.Length; i++)
 // {
 //     if (arr[i] > max)
